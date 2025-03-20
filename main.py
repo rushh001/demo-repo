bb95b488205d8865c2c73a11319fcd4a7bf1f376
mohdz <rushhinjam@gmail.com>
2025-03-20 13:10:52 +0530

    My second commit

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def sum_of_two_numbers(a, b):
 # Example usage
 num1 = 30
 num2 = 20
-# Call the function
+
+
+
+
 result = sum_of_two_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is {result}")
